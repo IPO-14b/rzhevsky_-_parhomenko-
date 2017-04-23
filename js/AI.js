74dd2f16b25ee1b2f2d94d62daff19605c2ab0c8
egrprh <egrprh>
2017-04-23 19:19:15 +0300

    Добавлена функция хода игрока

--- js/AI.js
+++ js/AI.js
@@ -5,3 +5,25 @@ $(function() {
     $('.uw').text(userWins);
     $('.ew').text(enemyWins);
 });
+
+var mystep = 1;
+var steps = 9;
+var cell_step =
+    [
+        0, 0, 0, 0, 0, 0, 0, 0, 0
+    ];
+
+function userStep(cell) {
+    if (mystep != 1)
+        alert('Сейчас не Ваш ход');
+    else {
+        var cell_num = cell.attr('id');
+        cell_num--;
+        if (cell_step[cell_num] > 0)
+            alert('Эта клетка уже занята');
+        else {
+            //TODO
+        }
+    }
+}
+
