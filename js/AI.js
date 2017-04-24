adceb8a7fd73237dc2c0987bfd0e5b60ad768ab2
Kirill499 <kirow4i@gmail.com>
2017-04-24 10:48:51 +0300

    Добавление финкции "ничья"

--- js/AI.js
+++ js/AI.js
@@ -5,6 +5,38 @@ $(function() {
     $('.uw').text(userWins);
     $('.ew').text(enemyWins);
 });
+function enemyStep() {
+    if (mystep == 1)
+        return;
+
+    attack = checkAttack();
+    if (attack == 'false') {
+        defence = checkDefence();
+        if (defence == 'false') {
+            do {
+                next_step = getRandomCell();
+            } while (cell_step[next_step-1] > 0);
+        } else {
+            next_step = defence;
+        }
+    } else {
+        next_step = attack;
+    }
+
+    $('#'+next_step).css('color', '#a00').html('O');
+    next_step--;
+    cell_step[next_step] = 2;
+    if (checkWin() == 'false') {
+        mystep = 1;
+        steps--;
+        if (steps < 1) {
+            alert('Ничья');
+            reset();
+            return 'false';
+        }
+    } else {
+        return 'false';
+    }
 $(function() {
     mystep = Math.floor(Math.random() * (1 + 1));
     if (mystep == 0)
