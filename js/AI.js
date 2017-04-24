7b0530c69a910cff190d25f949c9575025814ca8
Kirill499 <kirow4i@gmail.com>
2017-04-24 10:46:37 +0300

    Добавление клика

--- js/AI.js
+++ js/AI.js
@@ -5,3 +5,13 @@ $(function() {
     $('.uw').text(userWins);
     $('.ew').text(enemyWins);
 });
+$(function() {
+    mystep = Math.floor(Math.random() * (1 + 1));
+    if (mystep == 0)
+        enemyStep();
+    $('.cell').live('click', function() {
+        if (userStep($(this)) != 'false') {
+            enemyStep();
+        }
+    });
+});
