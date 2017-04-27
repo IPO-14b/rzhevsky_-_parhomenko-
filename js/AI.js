f3b4bea8a4c7d0f5ca1cbf29a82241e49e447a59
kita99999123 <kita99999123@gmail.com>
2017-04-27 16:44:29 +0300

    Перестановка функций в более удобном порядке

--- js/AI.js
+++ js/AI.js
@@ -1,11 +1,32 @@
 var userWins  = localStorage["userWins"]  ? localStorage["userWins"]  : 0;
 var enemyWins = localStorage["enemyWins"] ? localStorage["enemyWins"] : 0;
 
+var mystep = 1;
+var steps = 9;
+var cell_step =
+    [
+        0, 0, 0, 0, 0, 0, 0, 0, 0
+    ];
+
+function userStep(cell) {
+    if (mystep != 1)
+        alert('Сейчас не Ваш ход');
+    else {
+        var cell_num = cell.attr('id');
+        cell_num--;
+        if (cell_step[cell_num] > 0)
+            alert('Эта клетка уже занята');
+        else {
+            //TODO
+        }
+    }
+}
+
 $(function() {
     $('.uw').text(userWins);
     $('.ew').text(enemyWins);
 });
-<<<<<<< HEAD
+
 function enemyStep() {
     if (mystep == 1)
         return;
@@ -48,27 +69,3 @@ $(function() {
         }
     });
 });
-=======
-
-var mystep = 1;
-var steps = 9;
-var cell_step =
-    [
-        0, 0, 0, 0, 0, 0, 0, 0, 0
-    ];
-
-function userStep(cell) {
-    if (mystep != 1)
-        alert('Сейчас не Ваш ход');
-    else {
-        var cell_num = cell.attr('id');
-        cell_num--;
-        if (cell_step[cell_num] > 0)
-            alert('Эта клетка уже занята');
-        else {
-            //TODO
-        }
-    }
-}
-
->>>>>>> origin/master
