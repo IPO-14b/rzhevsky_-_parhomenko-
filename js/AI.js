58c91d9b4a8d0f8d2410c83bf4ce75c3e1589572
egrprh <egrprh@users.noreply.github.com>
2017-12-06 05:51:09 +0300

    Мелкие исправления
    Исправление ошибок форматирования кода и разрешение конфликтов

--- js/AI.js
+++ js/AI.js
@@ -4,9 +4,9 @@
 * Используем для определения счета побед пользователя
 * Содержит числовое значение
 *
-* @var		int userWins
+* @var        int userWins
 */
-var userWins  = localStorage["userWins"]  ? localStorage["userWins"]  : 0;
+var userWins = localStorage["userWins"] ? localStorage["userWins"] : 0;
 
 /**
 * Счет побед компьютера
@@ -14,7 +14,7 @@ var userWins  = localStorage["userWins"]  ? localStorage["userWins"]  : 0;
 * Используем для определения счета побед компьютера
 * Содержит числовое значение
 *
-* @var		int enemyWins
+* @var        int enemyWins
 */
 var enemyWins = localStorage["enemyWins"] ? localStorage["enemyWins"] : 0;
 
@@ -25,7 +25,7 @@ var enemyWins = localStorage["enemyWins"] ? localStorage["enemyWins"] : 0;
 * Содержит числовое значение между 0 и 1
 * Ход пользователя разрешен при значении 1 и запрещен при значении 0
 *
-* @var		int mystep
+* @var        int mystep
 */
 var mystep = 1;
 
@@ -35,7 +35,7 @@ var mystep = 1;
 * Используем для определения количества оставшихся ходов
 * Содержит числовое значение не болше количества ячеек в поле
 *
-* @var		int steps
+* @var        int steps
 */
 var steps = 9;
 
@@ -45,7 +45,7 @@ var steps = 9;
 * Используем для определения крестиков или ноливков в клетках
 * Содержит массив числовых значений 0 1 2
 *
-* @var		int[] cell_step
+* @var        int[] cell_step
 */
 var cell_step =
     [
@@ -61,7 +61,7 @@ var cell_step =
 * и проверяется победил пользователь или игра еще не окончена
 *
 * @param object cell Обьект ячейки на поле для отрисовки хода
-* @return string 	 Возвращает 'false' если результат игры ничья или победа пользователя
+* @return string      Возвращает 'false' если результат игры ничья или победа пользователя
 */
 function userStep(cell) {
     if (mystep != 1)
@@ -102,7 +102,7 @@ $(function() {
 * checkDefence определяющие нужно компьютеру блокировать линию ходов 
 * пользователя либо атаковать и совершается соответствующее действие
 *  
-* @return string 	 Возвращает 'false' если результат игры ничья или победа компьютера
+* @return string      Возвращает 'false' если результат игры ничья или победа компьютера
 */
 function enemyStep() {
     if (mystep == 1)
@@ -138,19 +138,16 @@ function enemyStep() {
     }
 }
 
-<<<<<<< HEAD
 /**
 * Проверка далжен ли компьютер блокировать серию ходов пользователя
 * 
 * Проверяется состояние ячеек на поле и есть 2 ячейки заполненные крестиками либо ноликами пользователя
 * то возвращается номер ячейки необходимый для блокирвоки хода пользователя.
 *
-* @return mixed 	 Возвращает номер ячейки необходимый для защиты либо false если угрозы нет
+* @return mixed      Возвращает номер ячейки необходимый для защиты либо false если угрозы нет
 */
-=======
->>>>>>> origin/master
-	function checkDefence() {
-	if ((cell_step[0] == 1 && cell_step[1] == 1 && cell_step[2] == 0) || (cell_step[5] == 1 && cell_step[8] == 1 && cell_step[2] == 0) || (cell_step[4] == 1 && cell_step[6] == 1 && cell_step[2] == 0))
+function checkDefence() {
+    if ((cell_step[0] == 1 && cell_step[1] == 1 && cell_step[2] == 0) || (cell_step[5] == 1 && cell_step[8] == 1 && cell_step[2] == 0) || (cell_step[4] == 1 && cell_step[6] == 1 && cell_step[2] == 0))
         return 3;
     if ((cell_step[0] == 1 && cell_step[2] == 1 && cell_step[1] == 0) || (cell_step[4] == 1 && cell_step[7] == 1 && cell_step[1] == 0))
         return 2;
@@ -169,22 +166,18 @@ function enemyStep() {
     if ((cell_step[7] == 1 && cell_step[8] == 1 && cell_step[6] == 0) || (cell_step[0] == 1 && cell_step[3] == 1 && cell_step[6] == 0) || (cell_step[2] == 1 && cell_step[4] == 1 && cell_step[6] == 0))
         return 7;
     return 'false';
-	}
-	
-<<<<<<< HEAD
+}
+    
 /**
 * Выбор ячейки для хода
 * 
 * Проверяется состояние ячеек на поле и есть 2 ячейки заполненные крестиками либо ноликами компьютера
 * то возвращается номер ячейки необходимый для хода для необходимой победы
 *
-* @return mixed 	 Возвращает номер ячейки необходимый для атаки либо false если таких ячеек нет
+* @return mixed      Возвращает номер ячейки необходимый для атаки либо false если таких ячеек нет
 */
-=======
-
->>>>>>> origin/master
 function checkAttack() {
-	if (cell_step[4] == 0)
+    if (cell_step[4] == 0)
     return 5;
 
     if ((cell_step[0] == 2 && cell_step[1] == 2 && cell_step[2] == 0) || (cell_step[5] == 2 && cell_step[8] == 2 && cell_step[2] == 0) || (cell_step[4] == 2 && cell_step[6] == 2 && cell_step[2] == 0))
@@ -206,17 +199,13 @@ function checkAttack() {
     if ((cell_step[7] == 2 && cell_step[8] == 2 && cell_step[6] == 0) || (cell_step[0] == 2 && cell_step[3] == 2 && cell_step[6] == 0) || (cell_step[2] == 2 && cell_step[4] == 2 && cell_step[6] == 0))
         return 7;
     return 'false';
-	}
+}
 
-<<<<<<< HEAD
 /**
 * Сброс поля
 * 
 * Очищается поле и игра начинается заново
 */
-=======
-
->>>>>>> origin/master
 function reset() {
     mystep = Math.floor(Math.random() * (1 + 1));
     steps = 9;
@@ -235,8 +224,7 @@ function reset() {
     if (mystep == 0)
         enemyStep();
 }
-	
-<<<<<<< HEAD
+    
 /**
 * Выбор случайной ячейки
 *
@@ -244,22 +232,15 @@ function reset() {
 * 
 * @return int Возвращает номер выбранной ячейки
 */
-=======
-
->>>>>>> origin/master
-	function getRandomCell() {
-    	return Math.floor(Math.random() * 9) + 1;
-	}
-	
-
-<<<<<<< HEAD
+function getRandomCell() {
+    return Math.floor(Math.random() * 9) + 1;
+}
+    
 /**
 * Определение хода противника или игрока
 * 
 * Определяет кто сейчас совершает ход
 */
-=======
->>>>>>> origin/master
 $(function() {
     mystep = Math.floor(Math.random() * (1 + 1));
     if (mystep == 0)
