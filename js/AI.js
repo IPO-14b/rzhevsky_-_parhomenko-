c1d6556a8644d4964bfc9f6d287b04f68c1694cd
egrprh <egrprh@users.noreply.github.com>
2017-12-08 05:32:52 +0300

    Форматирование кода
    Исправлено форматирование кода

--- js/AI.js
+++ js/AI.js
@@ -64,15 +64,17 @@ var cell_step =
 * @return string      Возвращает 'false' если результат игры ничья или победа пользователя
 */
 function userStep(cell) {
-    if (mystep != 1)
+    if (mystep != 1) {
         alert('Сейчас не Ваш ход');
+    }
     else {
         var cell_num = cell.attr('id');
         cell_num--;
-        if (cell_step[cell_num] > 0)
+        if (cell_step[cell_num] > 0) {
             alert('Эта клетка уже занята');
+        }
         else {
-             cell.css('color', '#0a0').html('X');
+            cell.css('color', '#0a0').html('X');
             cell_step[cell_num] = 1;
             if (checkWin() == 'false') {
                 mystep = 0;
@@ -105,8 +107,9 @@ $(function() {
 * @return string      Возвращает 'false' если результат игры ничья или победа компьютера
 */
 function enemyStep() {
-    if (mystep == 1)
+    if (mystep == 1) {
         return;
+    }
 
     attack = checkAttack();
     if (attack == 'false') {
@@ -147,24 +150,33 @@ function enemyStep() {
 * @return mixed      Возвращает номер ячейки необходимый для защиты либо false если угрозы нет
 */
 function checkDefence() {
-    if ((cell_step[0] == 1 && cell_step[1] == 1 && cell_step[2] == 0) || (cell_step[5] == 1 && cell_step[8] == 1 && cell_step[2] == 0) || (cell_step[4] == 1 && cell_step[6] == 1 && cell_step[2] == 0))
+    if ((cell_step[0] == 1 && cell_step[1] == 1 && cell_step[2] == 0) || (cell_step[5] == 1 && cell_step[8] == 1 && cell_step[2] == 0) || (cell_step[4] == 1 && cell_step[6] == 1 && cell_step[2] == 0)) {
         return 3;
-    if ((cell_step[0] == 1 && cell_step[2] == 1 && cell_step[1] == 0) || (cell_step[4] == 1 && cell_step[7] == 1 && cell_step[1] == 0))
+    }
+    if ((cell_step[0] == 1 && cell_step[2] == 1 && cell_step[1] == 0) || (cell_step[4] == 1 && cell_step[7] == 1 && cell_step[1] == 0)) {
         return 2;
-    if ((cell_step[1] == 1 && cell_step[2] == 1 && cell_step[0] == 0) || (cell_step[3] == 1 && cell_step[6] == 1 && cell_step[0] == 0) || (cell_step[4] == 1 && cell_step[8] == 1 && cell_step[0] == 0))
+    }
+    if ((cell_step[1] == 1 && cell_step[2] == 1 && cell_step[0] == 0) || (cell_step[3] == 1 && cell_step[6] == 1 && cell_step[0] == 0) || (cell_step[4] == 1 && cell_step[8] == 1 && cell_step[0] == 0)) {
         return 1;
-    if ((cell_step[3] == 1 && cell_step[4] == 1 && cell_step[5] == 0) || (cell_step[2] == 1 && cell_step[8] == 1 && cell_step[5] == 0))
+    }
+    if ((cell_step[3] == 1 && cell_step[4] == 1 && cell_step[5] == 0) || (cell_step[2] == 1 && cell_step[8] == 1 && cell_step[5] == 0)) {
         return 6;
-    if ((cell_step[3] == 1 && cell_step[5] == 1 && cell_step[4] == 0) || (cell_step[1] == 1 && cell_step[7] == 1 && cell_step[4] == 0))
+    }
+    if ((cell_step[3] == 1 && cell_step[5] == 1 && cell_step[4] == 0) || (cell_step[1] == 1 && cell_step[7] == 1 && cell_step[4] == 0)) {
         return 5;
-    if ((cell_step[4] == 1 && cell_step[5] == 1 && cell_step[3] == 0) || (cell_step[0] == 1 && cell_step[6] == 1 && cell_step[3] == 0))
-        return 4;
-    if ((cell_step[6] == 1 && cell_step[7] == 1 && cell_step[8] == 0) || (cell_step[2] == 1 && cell_step[5] == 1 && cell_step[8] == 0) || (cell_step[0] == 1 && cell_step[4] == 1 && cell_step[8] == 0))
+    }
+    if ((cell_step[4] == 1 && cell_step[5] == 1 && cell_step[3] == 0) || (cell_step[0] == 1 && cell_step[6] == 1 && cell_step[3] == 0)) {
+        return 4; 
+    }
+    if ((cell_step[6] == 1 && cell_step[7] == 1 && cell_step[8] == 0) || (cell_step[2] == 1 && cell_step[5] == 1 && cell_step[8] == 0) || (cell_step[0] == 1 && cell_step[4] == 1 && cell_step[8] == 0)) {
         return 9;
-    if ((cell_step[6] == 1 && cell_step[8] == 1 && cell_step[7] == 0) || (cell_step[1] == 1 && cell_step[4] == 1 && cell_step[7] == 0))
+    }
+    if ((cell_step[6] == 1 && cell_step[8] == 1 && cell_step[7] == 0) || (cell_step[1] == 1 && cell_step[4] == 1 && cell_step[7] == 0)) {
         return 8;
-    if ((cell_step[7] == 1 && cell_step[8] == 1 && cell_step[6] == 0) || (cell_step[0] == 1 && cell_step[3] == 1 && cell_step[6] == 0) || (cell_step[2] == 1 && cell_step[4] == 1 && cell_step[6] == 0))
+    }
+    if ((cell_step[7] == 1 && cell_step[8] == 1 && cell_step[6] == 0) || (cell_step[0] == 1 && cell_step[3] == 1 && cell_step[6] == 0) || (cell_step[2] == 1 && cell_step[4] == 1 && cell_step[6] == 0)) {
         return 7;
+    }
     return 'false';
 }
     
@@ -177,27 +189,37 @@ function checkDefence() {
 * @return mixed      Возвращает номер ячейки необходимый для атаки либо false если таких ячеек нет
 */
 function checkAttack() {
-    if (cell_step[4] == 0)
-    return 5;
+    if (cell_step[4] == 0) { 
+    	return 5;
+    }
 
-    if ((cell_step[0] == 2 && cell_step[1] == 2 && cell_step[2] == 0) || (cell_step[5] == 2 && cell_step[8] == 2 && cell_step[2] == 0) || (cell_step[4] == 2 && cell_step[6] == 2 && cell_step[2] == 0))
+    if ((cell_step[0] == 2 && cell_step[1] == 2 && cell_step[2] == 0) || (cell_step[5] == 2 && cell_step[8] == 2 && cell_step[2] == 0) || (cell_step[4] == 2 && cell_step[6] == 2 && cell_step[2] == 0)) {
         return 3;
-    if ((cell_step[0] == 2 && cell_step[2] == 2 && cell_step[1] == 0) || (cell_step[4] == 2 && cell_step[7] == 2 && cell_step[1] == 0))
+    }
+    if ((cell_step[0] == 2 && cell_step[2] == 2 && cell_step[1] == 0) || (cell_step[4] == 2 && cell_step[7] == 2 && cell_step[1] == 0)) {
         return 2;
-    if ((cell_step[1] == 2 && cell_step[2] == 2 && cell_step[0] == 0) || (cell_step[3] == 2 && cell_step[6] == 2 && cell_step[0] == 0) || (cell_step[4] == 2 && cell_step[8] == 2 && cell_step[0] == 0))
+    }
+    if ((cell_step[1] == 2 && cell_step[2] == 2 && cell_step[0] == 0) || (cell_step[3] == 2 && cell_step[6] == 2 && cell_step[0] == 0) || (cell_step[4] == 2 && cell_step[8] == 2 && cell_step[0] == 0)) {
         return 1;
-    if ((cell_step[3] == 2 && cell_step[4] == 2 && cell_step[5] == 0) || (cell_step[2] == 2 && cell_step[8] == 2 && cell_step[5] == 0))
+    }
+    if ((cell_step[3] == 2 && cell_step[4] == 2 && cell_step[5] == 0) || (cell_step[2] == 2 && cell_step[8] == 2 && cell_step[5] == 0)) {
         return 6;
-    if ((cell_step[3] == 2 && cell_step[5] == 2 && cell_step[4] == 0) || (cell_step[1] == 2 && cell_step[7] == 2 && cell_step[4] == 0))
+    }
+    if ((cell_step[3] == 2 && cell_step[5] == 2 && cell_step[4] == 0) || (cell_step[1] == 2 && cell_step[7] == 2 && cell_step[4] == 0)) {
         return 5;
-    if ((cell_step[4] == 2 && cell_step[5] == 2 && cell_step[3] == 0) || (cell_step[0] == 2 && cell_step[6] == 2 && cell_step[3] == 0))
+    }
+    if ((cell_step[4] == 2 && cell_step[5] == 2 && cell_step[3] == 0) || (cell_step[0] == 2 && cell_step[6] == 2 && cell_step[3] == 0)) {
         return 4;
-    if ((cell_step[6] == 2 && cell_step[7] == 2 && cell_step[8] == 0) || (cell_step[2] == 2 && cell_step[5] == 2 && cell_step[8] == 0) || (cell_step[0] == 2 && cell_step[4] == 2 && cell_step[8] == 0))
+    }
+    if ((cell_step[6] == 2 && cell_step[7] == 2 && cell_step[8] == 0) || (cell_step[2] == 2 && cell_step[5] == 2 && cell_step[8] == 0) || (cell_step[0] == 2 && cell_step[4] == 2 && cell_step[8] == 0)) {
         return 9;
-    if ((cell_step[6] == 2 && cell_step[8] == 2 && cell_step[7] == 0) || (cell_step[1] == 2 && cell_step[4] == 2 && cell_step[7] == 0))
+    }
+    if ((cell_step[6] == 2 && cell_step[8] == 2 && cell_step[7] == 0) || (cell_step[1] == 2 && cell_step[4] == 2 && cell_step[7] == 0)) {
         return 8;
-    if ((cell_step[7] == 2 && cell_step[8] == 2 && cell_step[6] == 0) || (cell_step[0] == 2 && cell_step[3] == 2 && cell_step[6] == 0) || (cell_step[2] == 2 && cell_step[4] == 2 && cell_step[6] == 0))
-        return 7;
+    }
+    if ((cell_step[7] == 2 && cell_step[8] == 2 && cell_step[6] == 0) || (cell_step[0] == 2 && cell_step[3] == 2 && cell_step[6] == 0) || (cell_step[2] == 2 && cell_step[4] == 2 && cell_step[6] == 0)) {
+        return 7; 
+    }
     return 'false';
 }
 
@@ -221,8 +243,9 @@ function reset() {
     localStorage["enemyWins"] = enemyWins;
     chrome.browserAction.setBadgeText({text: userWins + ":" + enemyWins});
 
-    if (mystep == 0)
+    if (mystep == 0) {
         enemyStep();
+    }
 }
     
 /**
@@ -243,8 +266,9 @@ function getRandomCell() {
 */
 $(function() {
     mystep = Math.floor(Math.random() * (1 + 1));
-    if (mystep == 0)
-        enemyStep();
+    if (mystep == 0) {
+        enemyStep(); 
+    }
     $('.cell').live('click', function() {
         if (userStep($(this)) != 'false') {
             enemyStep();
